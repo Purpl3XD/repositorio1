24595375a2963be7b36e2db6fd30d308746dc16c
H1G0R <higor.eberhardt@gmail.com>
2023-06-02 14:20:02 -0300

    :tada: initial commit

---
+++ JS/aula.js
@@ -0,0 +1,1 @@
+console.log('BEm vindos ao starter !!')
---
+++ JS/conceitos.js
@@ -0,0 +1,12 @@
+//* number  numeros podem ser inteiros reias ou float, nao numeros e Infinity 
+33//interios 12.4//reias  NaN// not a number  Infinty// infinito*/
+ 
+
+//* null - nulo*/
+ 
+/* object 
+  *Object
+  *propriedades 
+  *funcionlidades
+//   {propriedade: valor}
+*/
